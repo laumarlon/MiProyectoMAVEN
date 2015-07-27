36f0fcf30461210e6c0680a791192b2ab8b2793b
laura.martinezl <laura.martinezl@lis-sala4>
2015-07-27 15:01:47 -0500

    nuevo archivo

---
+++ src/main/java/org/glassfish/servlet/annotation_war/Prueba.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.glassfish.servlet.annotation_war;
+
+/**
+ *
+ * @author laura.martinezl
+ */
+public class Prueba {
+    
+}
